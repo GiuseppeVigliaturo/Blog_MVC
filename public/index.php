3d9019c94c1f59718f375bcc8108ce0f5c7fe6bb
giuviglia94 <vigliaturo94@gmail.com>
2021-12-16 19:55:09 +0100

    connessione al DB con PDO

--- public/index.php
+++ public/index.php
@@ -6,6 +6,12 @@ use App\Controllers\PostController;
 
 chdir(dirname(__DIR__));
 
+try {
+    $conn = new PDO('mysql:dbname=freeblog;host=localhost', 'root', '');
+} catch (\PDOException $e) {
+    die($e->getMessage());
+}
+
 require_once __DIR__."/../app/Controllers/PostController.php";
 
 $controller = new App\Controllers\PostController();

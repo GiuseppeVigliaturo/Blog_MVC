0e5a49d3c73fb48a5dac8cf5ad3760fc60c4f990
giuviglia94 <vigliaturo94@gmail.com>
2021-12-19 16:57:56 +0100

    funzione helpers per le  views

--- app/Controllers/PostController.php
+++ app/Controllers/PostController.php
@@ -2,21 +2,37 @@
 
 namespace App\Controllers;
 
+use App\Models\Post;
+use PDO;
+
 class PostController
 {
     protected $layout = 'layout/index.tpl.php';
-    public $name = 'Hello World';
     public $content;
     protected $conn;
-    public function __construct(\PDO $conn)
+    protected $Post;
+    public function __construct(PDO $conn)
     {
         $this->conn = $conn;
+        $this->Post = new Post($conn);
+    }
 
-        $posts = $this->conn->query('select * from posts')->fetchAll(\PDO::FETCH_OBJ);
-        ob_start();
-        require __DIR__ . '/../views/posts.tpl.php';
-        $this->content = ob_get_contents();
-        ob_end_clean();
+    public function process()
+    {
+        $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $url = trim($url, '/');
+        $tokens = explode('/', $url);
+
+        switch ($tokens[0]) {
+            case 'posts':
+                $this->content = call_user_func([$this, 'getPosts']);
+                break;
+            case 'post':
+                if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+                    $this->content = call_user_func([$this, 'show'], $tokens[1]);
+                    break;
+                }
+        }
     }
 
     /**
@@ -24,16 +40,25 @@ class PostController
      */
     public function display()
     {
-        require  $this->layout;
+        require $this->layout;
     }
+    public function getPosts()
+    {
+        $posts = $this->Post->all();
+        //con compact passiamo un array di posts che sarebbe la varabile $posts = $this->Post->all();
+        //Questo perchè viene ciclata nella view
+        return view('posts', compact('posts'));
+    }
+    /**
+     * @return string
+     */
     public function show(int $postid)
     {
         $message = ' this is a post message';
-        //comincia a catturare quell che viene inviato al server
         ob_start();
-        require_once __DIR__ . '/../Views/post.tpl.php';
-        //una volta importato il layout inietto all'interno quello catturato fino ad ora cioè $message
-        $this->content = ob_get_contents();
+        require_once __DIR__ . '/../views/post.tpl.php';
+        $content = ob_get_contents();
         ob_end_clean();
+        return $content;
     }
 }
--- public/index.php
+++ public/index.php
@@ -3,26 +3,24 @@
 //imposto la cartella di riferimento
 
 use App\Controllers\PostController;
-
+use App\DB\DbFactory;
 chdir(dirname(__DIR__));
 
-require_once __DIR__ . '/../app/Controllers/PostController.php';
+
 require_once __DIR__ . '/../DB/DBPDO.php';
 require_once __DIR__ . '/../DB/DbFactory.php';
 $data = require 'config/database.php';
-try {
-    $controller = new PostController(($pdoConn = App\DB\DbFactory::create($data))->getConn());
+require_once __DIR__ . '/../app/Controllers/PostController.php';
+require_once __DIR__ . '/../app/models/Post.php';
+require_once __DIR__ . '/../helpers/functions.php';
 
+
+try {
+
+    $conn = DbFactory::create($data)->getConn();
+    $controller = new PostController($conn);
+    $controller->process();
     $controller->display();
-    // $stm = $conn->query('select * from posts', \PDO::FETCH_ASSOC);
-    // if ($stm) {
-    //     foreach ($stm as $row) {
-    //         print_r($row);
-    //     }
-    //     //per mostrare errori che non sono eccezioni uso else semplicemente
-    // }else {
-    //     var_dump($conn->errorInfo());
-    // }
-} catch (\PDOException $e) {
-    echo $e->getMessage();
+} catch (PDOException $e) {
+    die($e->getMessage());
 }

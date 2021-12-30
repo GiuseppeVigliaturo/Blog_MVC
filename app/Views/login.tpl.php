86f591a7e50d6d10b219d460d9885b716ae2e5cd
giuviglia94 <vigliaturo94@gmail.com>
2021-12-30 18:44:15 +0100

    Model User funzione login e verify login

--- app/Views/login.tpl.php
+++ app/Views/login.tpl.php
@@ -2,6 +2,8 @@
     <div class='col-md-6 offset-3'>
         <h1>Sign in </h1>
         <form action='/auth/signin' method='POST'>
+
+        <input type="hidden" name='_csrf' value="<?= $token ?>">
             <div class='form-group'>
                 <label for='email'>Email</label>
                 <input class='form-control' name='email' type='email' value='' id='email' required>
--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,4 +1,7 @@
 <?php
+
+session_start();
+
 require_once __DIR__ . '/../helpers/functions.php';
 require_once __DIR__ . '/../DB/DBPDO.php';
 
